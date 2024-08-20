59dd1f66ef6118a21f34322dd52a9a52a538c3fa
H-unter <hkrugerilingworth@gmail.com>
2024-08-21 09:06:50 +1000

    Fix message format

--- picomms/src/udp_chat.cpp
+++ picomms/src/udp_chat.cpp
@@ -16,8 +16,8 @@
 int main(int argc, char *argv[])
 {
     char *input_function_name = argv[0];  // Points to the program name
-    char *input_host_name = argv[1];           // Points to the host argument
-    char *input_port_number = argv[2];           // Points to the port argument
+    char *input_host_name = argv[1];      // Points to the host argument
+    char *input_port_number = argv[2];    // Points to the port argument
 
     std::string client_username = "Hunter";
 
@@ -55,8 +55,8 @@ int main(int argc, char *argv[])
 
     // Set SO_BROADCAST to 1
     int broadcast_enable = 1;
-    bool is_broadcase_enable_set = setsockopt(my_socket, SOL_SOCKET, SO_BROADCAST, &broadcast_enable, sizeof(broadcast_enable)) != -1;
-    if (!is_broadcase_enable_set) {
+    bool is_broadcast_enable_set = setsockopt(my_socket, SOL_SOCKET, SO_BROADCAST, &broadcast_enable, sizeof(broadcast_enable)) != -1;
+    if (!is_broadcast_enable_set) {
         perror("Failed to set SO_BROADCAST");
         return 1;
     }
@@ -68,7 +68,6 @@ int main(int argc, char *argv[])
         perror("Failed to set SO_REUSEPORT");
         return 1;
     }
-
 
     // Bind the socket to the address 
     if (bind(my_socket, address->ai_addr, address->ai_addrlen) == -1) {
@@ -94,9 +93,16 @@ int main(int argc, char *argv[])
     int max_username_length = 32;
     int max_message_length = 240;
 
+    int input_buffer_size = 1024;
+    char input_buffer[input_buffer_size]; // make it extra big in case someone messes up lol
+
+    int output_buffer_length = max_username_length + 1 + max_message_length + 1;
+    char output_buffer[output_buffer_length]; // Username + null + message + null
+
     while (true){
         // Call poll() to wait for events
         int poll_result = poll(pfds, 2, -1); // -1 means wait indefinitely
+        
         bool is_poll_successful = poll_result != -1;
         if (!is_poll_successful) {
             perror("poll failed");
@@ -107,21 +113,27 @@ int main(int argc, char *argv[])
         bool is_event_on_stdin = pfds[0].revents & POLLIN;
         bool is_event_on_socket = pfds[1].revents & POLLIN;
 
-        if (is_event_on_stdin) { // send message
-            std::string input_message;
-            bool is_read_error = !std::getline(std::cin, input_message);
+        if (is_event_on_stdin) { // send message from terminal input
+            std::string output_message;
+            bool is_read_error = !std::getline(std::cin, output_message);
             if (is_read_error){
                 fprintf(stderr, "Failed to read input\n");
             }
-            std::string message_to_send = client_username + input_message;
-            if (input_message.length() > max_message_length) {
-                fprintf(stderr, "Message length exceeds the maximum allowed length of %d bytes.\n", max_message_length);
-            }
-            int message_buffer_length = max_message_length + 1 + max_message_length + 1;
-            char message_buffer[message_buffer_length]; // Username + null + message + null
-            snprintf(message_buffer, sizeof(message_buffer), "%s\0%s", client_username.c_str(), input_message.c_str());
 
-            ssize_t bytes_sent = sendto(my_socket, message_to_send.c_str(), message_to_send.length(), 0, address->ai_addr, address->ai_addrlen);
+            if (output_message.length() > max_message_length) {
+                fprintf(stderr, "Message length exceeds the maximum allowed length of %d bytes.\n", max_message_length);
+                return 1;
+            }
+
+            // Copy the username into the buffer
+            strncpy(output_buffer, client_username.c_str(), max_username_length);
+            output_buffer[client_username.length()] = '\0'; // Null terminator between username and message
+
+            // Copy the message into the buffer, after the null terminator
+            strncpy(output_buffer + client_username.length() + 1, output_message.c_str(), max_message_length);
+
+            // Send the message
+            ssize_t bytes_sent = sendto(my_socket, output_buffer, client_username.length() + 1 + output_message.length(), 0, address->ai_addr, address->ai_addrlen);
             bool is_message_sent = bytes_sent != -1;
             if (!is_message_sent) {
                 perror("Failed to send");
@@ -132,18 +144,47 @@ int main(int argc, char *argv[])
         }
 
         
-        if (is_event_on_socket) { // recieve message
-            char buffer[1024]; // make it extra big in case someone messes up lol
-            ssize_t bytes_received = read(my_socket, buffer, sizeof(buffer) - 1); // with room for a trailing null
-            bool is_message_received = bytes_received != -1;
+        if (is_event_on_socket) { // recieve message from the socket
+            size_t recieved_bytes = read(my_socket, input_buffer, sizeof(input_buffer) - 1); // with room for a trailing null
+            bool is_message_received = recieved_bytes != -1;
             if (!is_message_received) {
                 perror("Failed to receive");
                 close(my_socket);
                 freeaddrinfo(address);
                 return 1;
             }
-            buffer[bytes_received] = '\0';
-            printf("Received: %s\n", buffer);
+            
+            size_t message_content_index = 0;
+            
+            // Iterate through the string until the null terminator is found
+            // message: username'\0'message'\0'
+            while (message_content_index < recieved_bytes && input_buffer[message_content_index] != '\0') { 
+                message_content_index++;
+            }
+
+            size_t end_message_index = message_content_index + 1;
+            while (end_message_index < recieved_bytes && input_buffer[end_message_index] != '\0') { 
+                end_message_index++;
+            }
+
+            input_buffer[end_message_index] = '\0'; // Null terminate the message
+
+
+
+            bool is_message_format_valid = message_content_index < input_buffer_size - 1;
+            if (!is_message_format_valid) {
+                fprintf(stderr, "Error: Malformed message received.\n");
+                close(my_socket);
+                freeaddrinfo(address);
+                return 1;
+            }
+
+
+            char* message_username = input_buffer; // username is the first part of the message
+            char* message_content = input_buffer + message_content_index + 1; // message is the second part of the message, after a \0
+
+            // Print the message
+            printf("[%s] %s\n", message_username, message_content);
         }
     }
 
